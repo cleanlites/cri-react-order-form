1cf26743896a9fc2ce7dce389d75ccbf7bb9f6b6
albert.sigman <albert.sigman@zenecosystems.com>
2023-08-17 10:06:51 -0400

    update real endpoint

--- src/resources/fetch.js
+++ src/resources/fetch.js
@@ -1,34 +1,34 @@
 const username = process.env.REACT_APP_USERNAME;
 const password = process.env.REACT_APP_PASSWORD;
 // const api_url = "https://cleanlites.com/wp-json/gf/v2/forms/13";
-const api_url = "http://localhost:1234/api";
-// const api_url = "https://admin.cleanlites.com/api";
+// const api_url = "http://localhost:1234/api";
+const api_url = 'https://admin.cleanlites.com/api';
 export const getForm = async () => {
   return await fetch(api_url, {
     headers: {
-      authorization: `Basic ${btoa(username + ":" + password).toString(
-        "base64",
+      authorization: `Basic ${btoa(username + ':' + password).toString(
+        'base64'
       )}`,
     },
   }).then((result) => result.json());
 };
 
 export const submitForm = async (data) => {
-  console.log("submitting...");
+  console.log('submitting...');
   console.log(data);
-  return await fetch(api_url + "/forms/submit-order-form", {
-    method: "POST",
+  return await fetch(api_url + '/forms/submit-order-form', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
 };
 export const submitFormData = async (data, captchaToken) => {
-  return await fetch(api_url + "/forms/submit-order-form", {
-    method: "POST",
+  return await fetch(api_url + '/forms/submit-order-form', {
+    method: 'POST',
     headers: {
-      "x-captcha-token": captchaToken,
+      'x-captcha-token': captchaToken,
     },
     body: data,
   });
