b3b93d3fa2dabdfb4bf4ebee9d404688390560f3
albert.sigman <albert.sigman@zenecosystems.com>
2024-11-19 19:26:03 -0500

    update tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
@@ -8,4 +8,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-})
+});
